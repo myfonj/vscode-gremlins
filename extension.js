8952fd54086788b585f5cfe87aeef9298146f6e2
Michal Čaplygin <michal.caplygin@etnetera.cz>
2017-03-06 01:52:36 +0100

    less boilerplate, more characters

--- extension.js
+++ extension.js
@@ -1,6 +1,61 @@
-var vscode = require('vscode');
+const vscode = require('vscode');
 
 function activate(context) {
+
+    const gremlist = [{
+        name: "zero-width space",
+        charpoint: 0x200b,
+        decorstyle: {
+            borderWidth: '1px',
+            borderStyle: 'solid',
+            borderColor: 'red'
+        },
+        // properties below will be set later
+        vsdecorstyle: null, // merged with base
+        decors: [], // "instances"
+        charpointstring: null // for hint
+    }, {
+        name: "non-breaking space",
+        charpoint: 0x00a0
+    }, {
+        name: "zero width no-break space, BOM",
+        charpoint: 0xfeff,
+        decorstyle: {
+            borderWidth: '3px',
+            borderStyle: 'solid'
+        }
+    }, {
+        name: 'word joiner',
+        charpoint: 0x2060,
+        decorstyle: {
+            borderWidth: '3px',
+            borderStyle: 'solid'
+        }
+    }];
+
+    const basicdecorstyle = {
+        borderWidth: '1px',
+        borderStyle: 'none',
+        borderColor: 'red',
+        backgroundColor: 'rgba(255,128,128,.5)',
+        overviewRulerColor: 'darkred',
+        overviewRulerLane: vscode.OverviewRulerLane.Right,
+        light: {
+            gutterIconPath: context.asAbsolutePath('images/gremlins-light.svg'),
+        },
+        dark: {
+            gutterIconPath: context.asAbsolutePath('images/gremlins-dark.svg'),
+        },
+        gutterIconSize: 'contain'
+    };
+
+    gremlist.forEach((gremitem) => {
+        let style = Object.assign({}, basicdecorstyle, gremitem.decorstyle);
+        gremitem.charpointstring = 'U+'+gremitem.charpoint.toString(16).replace(/.*/,m=>m.length<4?'0'.repeat(4-m.length)+m:m);
+        gremitem.vsdecorstyle = vscode.window.createTextEditorDecorationType(style);
+        gremitem.regexp = RegExp(String.fromCodePoint(gremitem.charpoint) + '+', 'g')
+    });
+
     vscode.window.onDidChangeActiveTextEditor(editor => {
         if (!editor) {
             return;
@@ -22,67 +77,38 @@ function activate(context) {
         updateDecorations(vscode.window.activeTextEditor);
     }, null, context.subscriptions);
 
-    const zeroWidthSpaceDecorationType = vscode.window.createTextEditorDecorationType({
-        borderWidth: '1px',
-        borderStyle: 'solid',
-        borderColor: 'red',
-        overviewRulerColor: 'darkred',
-        overviewRulerLane: vscode.OverviewRulerLane.Right,
-        light: {
-            gutterIconPath: context.asAbsolutePath('images/gremlins-light.svg'),
-        },
-        dark: {
-            gutterIconPath: context.asAbsolutePath('images/gremlins-dark.svg'),
-        },
-        gutterIconSize: 'contain'
-    });
-
-    const nonBreakingSpaceDecorationType = vscode.window.createTextEditorDecorationType({
-        backgroundColor: 'rgba(255,128,128,.5)',
-        overviewRulerColor: 'darkred',
-        overviewRulerLane: vscode.OverviewRulerLane.Right,
-        light: {
-            gutterIconPath: context.asAbsolutePath('images/gremlins-light.svg'),
-        },
-        dark: {
-            gutterIconPath: context.asAbsolutePath('images/gremlins-dark.svg'),
-        },
-        gutterIconSize: 'contain'
-    });
-
     function updateDecorations(activeTextEditor) {
         if (!activeTextEditor) {
             return;
         }
 
-        const zeroWidthSpaceRegEx = /\u200b+/g;
-        const nonBreakingSpaceRegEx = /\u00a0+/g;
-
         const doc = activeTextEditor.document;
-        const decorationOptions = {
-            zeroWidthSpace: [],
-            nonBreakingSpace: []
-        };
-        for (let i = 0; i < doc.lineCount; i++) {
-            let lineText = doc.lineAt(i);
-            let line = lineText.text;
-            let match;
-            while (match = zeroWidthSpaceRegEx.exec(line)) {
-                let startPos = new vscode.Position(i, match.index);
-                let endPos = new vscode.Position(i, match.index + match[0].length);
-                const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: match[0].length + " zero-width space" + (match[0].length > 1 ? "s" : "") + " (unicode U+200b) here"};
-                decorationOptions.zeroWidthSpace.push(decoration);
-            }
-            while (match = nonBreakingSpaceRegEx.exec(line)) {
-                let startPos = new vscode.Position(i, match.index);
-                let endPos = new vscode.Position(i, match.index + match[0].length);
-                const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: match[0].length + " non-breaking space" + (match[0].length > 1 ? "s" : "") + " (unicode U+00a0) here"};
-                decorationOptions.nonBreakingSpace.push(decoration);
-            }
-        }
 
-        activeTextEditor.setDecorations(zeroWidthSpaceDecorationType, decorationOptions.zeroWidthSpace);
-        activeTextEditor.setDecorations(nonBreakingSpaceDecorationType, decorationOptions.nonBreakingSpace);
+        gremlist.forEach((gremitem) => {
+            gremitem.decorations = []
+        });
+
+        for (let i = 0; i < doc.lineCount; i++) {
+            let line = doc.lineAt(i);
+            let lineText = line.text;
+            let match;
+            gremlist.forEach((gremitem) => {
+                while (match = gremitem.regexp.exec(lineText)) {
+                    let startPos = new vscode.Position(i, match.index);
+                    let endPos = new vscode.Position(i, match.index + match[0].length);
+                    const decoration = {
+                        range: new vscode.Range(startPos, endPos),
+                        hoverMessage: match[0].length + " " + gremitem.name + (match[0].length > 1 ? "s" : "") + 
+                        " (unicode " + gremitem.charpointstring + ") here"
+                    };
+                    gremitem.decorations.push(decoration);
+                }
+            })
+        }
+        gremlist.forEach(gremitem => {
+            activeTextEditor.setDecorations(gremitem.vsdecorstyle, gremitem.decorations);
+        })
+
     }
 
     updateDecorations(vscode.window.activeTextEditor);
@@ -90,6 +116,5 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
